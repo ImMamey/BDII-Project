d67b27fed250bd6bfeb0f994a0f95c4d52f0a672
ImMamey <32584037+ImMamey@users.noreply.github.com>
2020-12-11 21:30:55 -0400

    Update Simulacion.sql

--- Simulacion/Simulacion.sql
+++ Simulacion/Simulacion.sql
@@ -3,16 +3,15 @@
 
 
 ---===============================================================
-CREATE OR REPLACE FUNCTION start_race(competitor E_P.fk_equipo_id%TYPE) RETURNS TABLE(
-    team_name EQUIPO.nombre%TYPE,
-    race_position BIGINT,
-    distance FLOAT,
-    loops BIGINT,
-    issues BIGINT,
-) as 
+CREATE OR REPLACE PROCEDURE start_race(competitor E_P.fk_equipo_id%TYPE) as 
 $func$
 DECLARE
-     r E_P%ROWTYPE;
+     r E_P%ROWTYPE; --fila
+     --a geto
+ 
+     competidores CURSOR FOR SELECT *
+       FROM E_P tabla
+       ORDER BY tabla.fk_equipo_id;
 BEGIN
  --init variables
   competitor_id := 0;
@@ -20,13 +19,13 @@ BEGIN
   tiempo_m:=0 BIGINT;
   tiempo_s:=0 BIGINT;
 
-  FOR competitor_id IN 
-       SELECT *
-       FROM E_P tabla
-       ORDER BY tabla.fk_equipo_id 
-  LOOP
-     ---CODIGO AQUI
-     --- SELECT "startTimer"()
+  FOR E_P IN competidores LOOP
+     /*
+     If competidor tiempo is <=24h then:
+     */
+     SELECT "startTimer"() -- por cada competidor
+     -- else
+    --- asdfasdfasdf
   END LOOP;
 
 RETURNS TABLE; 
@@ -36,8 +35,8 @@ $func$LANGUAGE plpgsql;
 ---================================
 
 
-CREATE OR REPLACE FUNCTION startTimer()
-RETURNS voids as $func$
+CREATE OR REPLACE FUNCTION startTimer(%ROWTYPE)
+RETURNS voids as $teamito$
 BEGIN
 END;
-$func$ LANGUAGE plpgsql;
+$teamito$ LANGUAGE plpgsql;
