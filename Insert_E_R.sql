bbf5608b5f44e91e17f5e3f89ee8920ed38d24de
LuisPinto17 <lapinto.17@est.ucab.edu.ve>
2020-12-15 16:13:40 -0400

    cambios de tablas
    cambio de tda

--- Insert_E_R.sql
+++ Insert_E_R.sql
@@ -1,24 +1,24 @@
 INSERT INTO E_R (categoria, numero_equipo, marca_cauchos, fk_ranking_id, foto, fk_ranking_evento_id, fk_e_p_fk_piloto_id, fk_e_p_fk_equipo_id) VALUES
     ('3001-5000',5,'firestone',-,
     '',
-    -,-,-);
+    1,-,-);
 
 INSERT INTO E_R (categoria, numero_equipo, marca_cauchos, fk_ranking_id, foto, fk_ranking_evento_id, fk_e_p_fk_piloto_id, fk_e_p_fk_equipo_id) VALUES
     ('3001-5000',7,'michelin',-,
     '',
-    -,-,-);
+    1,-,-);
 
 INSERT INTO E_R (categoria, numero_equipo, marca_cauchos, fk_ranking_id, foto, fk_ranking_evento_id, fk_e_p_fk_piloto_id, fk_e_p_fk_equipo_id) VALUES
     ('5001-8000',4,'pirelli',-,
     '',
-    -,-,-);
+    1,-,-);
 
 INSERT INTO E_R (categoria, numero_equipo, marca_cauchos, fk_ranking_id, foto, fk_ranking_evento_id, fk_e_p_fk_piloto_id, fk_e_p_fk_equipo_id) VALUES
     ('3001-5000',14,'pirelli',-,
     '',
-    -,-,-);
+    1,-,-);
 
 INSERT INTO E_R (categoria, numero_equipo, marca_cauchos, fk_ranking_id, foto, fk_ranking_evento_id, fk_e_p_fk_piloto_id, fk_e_p_fk_equipo_id) VALUES
     ('2001-3000',19,'firestone',-,
     '',
-    -,-,-);
+    1,-,-);
--- tables.sql
+++ tables.sql
@@ -16,9 +16,9 @@ ano BIGINT
 );
 
 CREATE TYPE TIEMPO AS(
-hora BIGINT,
 minuto BIGINT,
-segundo BIGINT
+segundo BIGINT,
+centesimas BIGINT
 );
 ---========== TABLE's================
 CREATE TABLE MARCA_AUTO(
@@ -126,7 +126,7 @@ ALTER TABLE P_S ADD CONSTRAINT p_s_pista_id_fk FOREIGN KEY (fk_pista_id) REFEREN
 CREATE TABLE SUCESO(
  id BIGINT NOT NULL,
  tipo_suceso VARCHAR NOT NULL,
- momento_suceso TIEMPO,  --TDA TIEMPO
+ momento_suceso INT, 
  clima_momento VARCHAR(4) NOT NULL,
  causa VARCHAR(3),
  tipo_bandera VARCHAR,
@@ -138,9 +138,10 @@ CREATE TABLE SUCESO(
 ALTER TABLE SUCESO ADD CONSTRAINT suceso_p_s_id_fk FOREIGN KEY (fk_p_s_fk_seccion_id,fk_p_s_fk_pista_id) REFERENCES P_S (fk_seccion_id,fk_pista_id);
 
 CREATE TABLE E_P(
+ id BIGINT NOT NULL,
  fk_piloto_id BIGINT NOT NULL,
  fk_equipo_id BIGINT NOT NULL,
- PRIMARY KEY(fk_piloto_id,fk_equipo_id)
+ PRIMARY KEY(id)
 );
 
 ALTER TABLE E_P ADD CONSTRAINT e_p_piloto_id_fk FOREIGN KEY (fk_piloto_id) REFERENCES PILOTO (id);
@@ -185,18 +186,16 @@ CREATE TABLE E_R(
     fk_ranking_id BIGINT NOT NULL,
     foto BYTEA NOT NULL,
     fk_ranking_evento_id BIGINT NOT NULL,
-    fk_e_p_fk_piloto_id BIGINT NOT NULL,
-    fk_e_p_fk_equipo_id BIGINT NOT NULL,
+    fk_e_p_id BIGINT NOT NULL,
     PRIMARY KEY(
         fk_ranking_id,
         fk_ranking_evento_id,
-        fk_e_p_fk_piloto_id,
-        fk_e_p_fk_equipo_id
+        fk_e_p_id
         )
 );
 
 ALTER TABLE E_R ADD CONSTRAINT e_r_ranking_fk FOREIGN KEY (fk_ranking_id,fk_ranking_evento_id) REFERENCES RANKING (id,fk_evento_id);
-ALTER TABLE E_R ADD CONSTRAINT e_r_p_id_fk FOREIGN KEY (fk_e_p_fk_piloto_id,fk_e_p_fk_equipo_id) REFERENCES E_P (fk_piloto_id,fk_equipo_id);
+ALTER TABLE E_R ADD CONSTRAINT e_r_p_id_fk FOREIGN KEY (fk_e_p_id) REFERENCES E_P (id);
 
 
 CREATE TABLE RANKING_HORA(
@@ -205,17 +204,15 @@ CREATE TABLE RANKING_HORA(
     puesto BIGINT NOT NULL,
     fk_e_r_ranking_id BIGINT NOT NULL,
     fk_e_r_ranking_evento_id BIGINT NOT NULL,
-    fk_e_r_fk_piloto_id BIGINT NOT NULL,
-    fk_e_r_fk_equipo_id BIGINT NOT NULL,
+    fk_e_r_id BIGINT NOT NULL,
         PRIMARY KEY(
         id,
         fk_e_r_ranking_id,
         fk_e_r_ranking_evento_id,
-        fk_e_r_fk_piloto_id,
-        fk_e_r_fk_equipo_id
+        fk_e_r_id
         )
 );
 
-ALTER TABLE RANKING_HORA ADD CONSTRAINT hora_e_r_id_fk FOREIGN KEY (fk_e_r_ranking_id,fk_e_r_ranking_evento_id,fk_e_r_fk_piloto_id,fk_e_r_fk_equipo_id) REFERENCES E_R (fk_ranking_id,fk_ranking_evento_id,fk_e_p_fk_piloto_id,fk_e_p_fk_equipo_id);
+ALTER TABLE RANKING_HORA ADD CONSTRAINT hora_e_r_id_fk FOREIGN KEY (fk_e_r_ranking_id,fk_e_r_ranking_evento_id,fk_e_r_id) REFERENCES E_R (fk_ranking_id,fk_ranking_evento_id,fk_e_p_id);
 
 
